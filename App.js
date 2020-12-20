8bd968103b64befd63731824bfac28ab3b1b731e
jbrietzke <jack.brietzke@gmail.com>
2020-12-19 22:03:49 -0500

    Added Redux practice and React Hooks

--- App.js
+++ App.js
@@ -5,20 +5,29 @@ import HomeScreen from './src/components/homeScreen'
 import ConsultScreen from './src/components/consultScreen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider, connect } from 'react-redux'
+import rootReducer from './src/store/configureStore'
+import countReducer from './src/reducers/countReducer'
+import { createStore } from 'redux'
+import configureStore from './src/store/configureStore';
 
 const Stack = createStackNavigator()
+const store = configureStore()
 
 
 
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <Provider store = {store}>
+      <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name='Home' component={HomeScreen}/>
         <Stack.Screen name="Consult" component={ConsultScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+      </NavigationContainer>
+    </Provider>
+    
     
   );
 }
